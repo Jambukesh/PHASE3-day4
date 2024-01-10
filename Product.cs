58b12453b74eaa6d2652d079926619d74d5e21d9
Jambukesh <jkesh360@gmail.com>
2024-01-10 15:04:17 +0200

    products

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandleProjects
+{
+    internal class Product
+    {
+        public string Pname { get; set; }
+        public int Pprice { get; set; }
+        public string? Pbrand { get; set; }
+        public DateTime ManufacturingDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HandleProjects;
+//Console.WriteLine("Hello, World!");
+
+List<Product > products = new List<Product >()
+{
+new Product(){ Pname="laptop",Pprice  =25000,Pbrand  ="HP",ManufacturingDate=new DateTime (day:12,month:11,year:2022),ExpiryDate=new DateTime(day:12,month:11,year:2030)},
+new Product(){ Pname="Mobile",Pprice  =45000,Pbrand  ="OPPO",ManufacturingDate=new DateTime (day:15,month:12,year:2021),ExpiryDate=new DateTime(day:12,month:11,year:2030)},
+new Product(){ Pname="SmartWatch",Pprice  =2000,Pbrand  ="Itel",ManufacturingDate=new DateTime (day:12,month:11,year:2022),ExpiryDate=new DateTime(day:12,month:11,year:2030)},
+new Product(){ Pname="Shoe",Pprice  =5000,Pbrand  ="Nike",ManufacturingDate=new DateTime (day:12,month:11,year:2022),ExpiryDate=new DateTime(day:12,month:11,year:2024)},
+
+};
+Console.WriteLine("Pname\tPprice\tPbrand\tMfdate\tExpdate");
+foreach (Product  pd in products )
+{
+    Console.Write(pd.Pname  + "\t\t");
+    Console.Write(pd.Pprice  + "\t\t");
+    Console.Write(pd.Pbrand  + "\t\t");
+    Console.Write(pd.ManufacturingDate .ToLongDateString() + "\t\t");
+    Console.Write(pd.ExpiryDate .ToLongDateString() + "\t\t");
+    Console.Write("\n");
+
+}
+
